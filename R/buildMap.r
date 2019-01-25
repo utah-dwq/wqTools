aabfbf02942a129496509d362085a43383bba249
jakevl <39382528+jakevl@users.noreply.github.com>
2019-01-25 11:00:21 -0700

    Searchable map features - AUs & sites

--- R/buildMap.r
+++ R/buildMap.r
@@ -2,14 +2,18 @@
 #' 
 #' Build a map of sample sites, facilities, or both. Map includes sites, beneficial use and assessment unit polygons, and satellite and topo baselayers.
 #' This is designed to work with column names as extracted from WQP or ECHO via udwqTools functions readWQP() and readECHO_fac(). Map will launch in default browser (or R-Studio's browser if using R-Studio).
+#' Site and assessment unit features are searchable by idetifier and name via the search button on the left side of the map.
+#' The most recently turned on layer is "on top" of the map. Only features on top will show their pop-up on click.
 #' @param fac Facility locations queried via readECHO_fac.
 #' @param sites Site locations queried via readWQP(type="sites"). May also be a data file with WQP site information merged to it.
-#' @param au_poly Optional. Polygon file to be mapped as assessment units. Useful for only mapping a subset of specific assessment units. If missing, the default state wide AU polygon is used.
-#' @param bu_poly Optional. Polygon file to be mapped as beneficial uses. Useful for only mapping a subset of beneficial uses. If missing, the default state wide uses polygon is used.
-#' @param ss_poly Optional. Polygon file to be mapped as site specific standards. Useful for only mapping a subset of ss polygons. If missing, the default state wide ss polygon is used.
+#' @param au_poly Optional. Polygon file to be mapped as assessment units. Useful for mapping a subset of specific assessment units. If missing, the default state wide AU polygon is used.
+#' @param bu_poly Optional. Polygon file to be mapped as beneficial uses. Useful for mapping a subset of beneficial uses. If missing, the default state wide uses polygon is used.
+#' @param ss_poly Optional. Polygon file to be mapped as site specific standards. Useful for mapping a subset of ss polygons. If missing, the default state wide ss polygon is used.
 #' @import leaflet
-#' @importFrom mapview addFeatures
+#' @importFrom leaflet.extras addSearchFeatures
 #' @importFrom RColorBrewer brewer.pal
+#' @importFrom sf st_centroid
+#' @importFrom sf st_coordinates
 #' @examples
 #' # Read sites & facility locations
 #' jr_sites=readWQP(type="sites",
@@ -32,14 +36,26 @@ buildMap=function(fac, sites, au_poly, bu_poly, ss_poly){
 	if(missing(au_poly)){get(data("au_poly", envir = environment()))}
 	if(missing(bu_poly)){get(data("bu_poly", envir = environment()))}
 	if(missing(ss_poly)){get(data("ss_poly", envir = environment()))}
+	au_centroids=suppressWarnings(sf::st_centroid(au_poly))
+	au_centroids=cbind(au_centroids,sf::st_coordinates(au_centroids))
 	
 	if(missing(fac) & missing(sites)){
 		print("Building map w/o sites or facilities...")
 		#Build empty map
 		
-		map=leaflet::leaflet(bu_poly)
+		map=leaflet::leaflet(au_poly)
 			map=leaflet::addProviderTiles(map, "Esri.WorldTopoMap", group = "Topo")
 			map=leaflet::addProviderTiles(map,"Esri.WorldImagery", group = "Satellite")
+			map=leaflet::addCircles(map, lat=au_centroids$Y, lng=au_centroids$X, group="au_names", label=au_centroids$AU_NAME, stroke=F, fill=F,
+				popup = paste0(
+					"AU ID: ", au_centroids$ASSESS_ID,
+					"<br> AU Name: ", au_centroids$AU_NAME,
+					"<br> AU Type: ", au_centroids$AU_Type))
+			map=leaflet::addCircles(map, lat=au_centroids$Y, lng=au_centroids$X, group="au_ids", label=au_centroids$ASSESS_ID, stroke=F, fill=F,
+				popup = paste0(
+					"AU name: ", au_centroids$AU_NAME,
+					"<br> AU ID: ", au_centroids$ASSESS_ID,
+					"<br> AU type: ", au_centroids$AU_Type))
 			map=addPolygons(map, data=bu_poly,group="Beneficial uses",smoothFactor=4,fillOpacity = 0.1,weight=3,color="green",
 				popup=paste0(
 					"Description: ", bu_poly$R317Descrp,
@@ -61,6 +77,13 @@ buildMap=function(fac, sites, au_poly, bu_poly, ss_poly){
 			map=hideGroup(map, "Assessment units")
 			map=hideGroup(map, "Site-specific standards")
 			map=hideGroup(map, "Beneficial uses")
+			#map=addControl(map, "<P><B>Search</B>", position='topleft')
+			map=leaflet.extras::addSearchFeatures(map,
+				targetGroups = c('au_ids','au_names'),
+				options = leaflet.extras::searchFeaturesOptions(
+				zoom=12, openPopup = TRUE, firstTipSubmit = TRUE,
+				autoCollapse = TRUE, hideMarkerOnCollapse = TRUE ))
+			map=leaflet::addMeasure(map, position="bottomleft")
 
 	}else{
 		if(!missing(sites)){
@@ -93,7 +116,7 @@ buildMap=function(fac, sites, au_poly, bu_poly, ss_poly){
 	
 		#Color palette for points
 		
-		pal <- colorRampPalette(RColorBrewer::brewer.pal(8, "Dark2"))
+		pal <- colorRampPalette(RColorBrewer::brewer.pal(11, "Spectral"))
 		pal=leaflet::colorFactor(pal(length(unique(locs$locationType))), domain = locs$locationType)
 		
 		#Build map
@@ -101,13 +124,37 @@ buildMap=function(fac, sites, au_poly, bu_poly, ss_poly){
 		map=leaflet::leaflet(locs)
 			map=leaflet::addProviderTiles(map, "Esri.WorldTopoMap", group = "Topo")
 			map=leaflet::addProviderTiles(map,"Esri.WorldImagery", group = "Satellite")
-			map=mapview::addFeatures(map, locs, group="Sites", color = pal(locs$locationType),
+			map=leaflet::addCircleMarkers(map, lat=locs$LatitudeMeasure, lng=locs$LongitudeMeasure, group="Sites", color = pal(locs$locationType), opacity=0.8,
 				popup = paste0(
 					"Location ID: ", locs$locationID,
 					"<br> Name: ", locs$locationName,
 					"<br> Type: ", locs$locationType,
 					"<br> Lat: ", locs$LatitudeMeasure,
 					"<br> Long: ", locs$LongitudeMeasure))
+			map=leaflet::addCircles(map, lat=locs$LatitudeMeasure, lng=locs$LongitudeMeasure, group="locationID", label=locs$locationID, stroke=F, fill=F,
+				popup = paste0(
+					"Location ID: ", locs$locationID,
+					"<br> Name: ", locs$locationName,
+					"<br> Type: ", locs$locationType,
+					"<br> Lat: ", locs$LatitudeMeasure,
+					"<br> Long: ", locs$LongitudeMeasure))
+			map=leaflet::addCircles(map, lat=locs$LatitudeMeasure, lng=locs$LongitudeMeasure, group="locationName", label=locs$locationName, stroke=F, fill=F,
+				popup = paste0(
+					"Location ID: ", locs$locationID,
+					"<br> Name: ", locs$locationName,
+					"<br> Type: ", locs$locationType,
+					"<br> Lat: ", locs$LatitudeMeasure,
+					"<br> Long: ", locs$LongitudeMeasure))
+			map=leaflet::addCircles(map, lat=au_centroids$Y, lng=au_centroids$X, group="au_names", label=au_centroids$AU_NAME, stroke=F, fill=F,
+				popup = paste0(
+					"AU ID: ", au_centroids$ASSESS_ID,
+					"<br> AU Name: ", au_centroids$AU_NAME,
+					"<br> AU Type: ", au_centroids$AU_Type))
+			map=leaflet::addCircles(map, lat=au_centroids$Y, lng=au_centroids$X, group="au_ids", label=au_centroids$ASSESS_ID, stroke=F, fill=F,
+				popup = paste0(
+					"AU name: ", au_centroids$AU_NAME,
+					"<br> AU ID: ", au_centroids$ASSESS_ID,
+					"<br> AU type: ", au_centroids$AU_Type))
 			map=leaflet::addLabelOnlyMarkers(map, group="Labels", lat=locs$LatitudeMeasure, lng=locs$LongitudeMeasure,
 				label=locs$locationID,labelOptions = leaflet::labelOptions(noHide = T, textsize = "15px"),
 				clusterOptions=leaflet::markerClusterOptions(spiderfyOnMaxZoom=T))
@@ -135,11 +182,16 @@ buildMap=function(fac, sites, au_poly, bu_poly, ss_poly){
 			map=hideGroup(map, "Assessment units")
 			map=hideGroup(map, "Site-specific standards")
 			map=hideGroup(map, "Beneficial uses")
+			#map=addControl(map, "<P><B>Search</B>", position='topleft')
+			map=addSearchFeatures(map,
+				targetGroups = c('locationID','locationName','au_ids','au_names'),
+				options = leaflet.extras::searchFeaturesOptions(
+				zoom=12, openPopup = TRUE, firstTipSubmit = TRUE,
+				autoCollapse = TRUE, hideMarkerOnCollapse = TRUE ))
+			map=leaflet::addMeasure(map, position="bottomleft")
 	}
 	
 return(map)
 
 }
-
-buildMap()
 
