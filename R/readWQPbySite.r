8cf961735f5bbbb41437323c04a556ac1359926e
jakevl <39382528+jakevl@users.noreply.github.com>
2019-01-28 13:00:05 -0700

    Update readWQPbySite.r

--- R/readWQPbySite.r
+++ R/readWQPbySite.r
@@ -4,15 +4,21 @@
 #' 
 #' 
 #' @export
-readWQPbySite=function(sites, map=buildMap(sites=sites), types=c('narrowresult', 'activity', 'sites'), ...)
+readWQPbySite=function(sites, map=buildMap(sites=sites), mode = "click", types=c('narrowresult', 'activity', 'sites'), merge=T, ...)
 	if(missing(sites)){
-		sites=readWQP(type="sites", statecode="US:49")
+		sites=readWQP(type="sites", statecode="US:49", ...)
 		sites=sites[sites$MonitoringLocationTypeName %in% c("Aggregate surface-water-use","Facility","Lake, Reservoir, Impoundment","Spring","Stream","Wetland"),]
 	}
 	
 	sites_sf=sf::st_as_sf(sites, coords=c("LongitudeMeasure","LatitudeMeasure"))
-	selection=mapedit::selectFeatures(sites_sf, map=map, index=T)
+	selection=mapedit::selectFeatures(sites_sf, map=map, index=T, mode=mode)
 	siteids_sel=sites$MonitoringLocationIdentifier[selection]
+	
+	result=list()
+	for(n in 1:length(types)){
+		res_n=readWQP(type=paste(types[n]),siteid=as.vector(siteids_sel), ...)
+		result=append(result, res_n)
+	}
 	
 	
 	
