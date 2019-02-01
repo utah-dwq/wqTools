63a5f24e5bcf209cf3d16be7c09d6faeb0b24257
jakevl <39382528+jakevl@users.noreply.github.com>
2019-02-01 14:05:30 -0700

    Update profileHeatMap.r

--- R/profileHeatMap.r
+++ R/profileHeatMap.r
@@ -11,12 +11,12 @@
 #' @param min_date Minimum plot date. 'YYYY-mm-dd' format.
 #' @param max_date Maximum plot date. 'YYYY-mm-dd' format.
 #' @param show_dates Logical. If TRUE (default), show individual profile dates on plot x-axis.
-#' @param criteria Vector of criteria values to be used as contours on heatmap plot.
+#' @param criteria Vector of criteria values to be used as contours on heatmap plot. If not specified, contours from 0-30 at increments of 5 are drawn.
 #' @importFrom akima interp
 #' @importFrom lubridate origin
 
 #' @export
-profileHeatMap=function(data, parameter, depth="Depth_m", param_units, depth_units="m", date=as.Date(data$ActivityStartDate), show_dates=TRUE, min_date=min(data[,date], na.rm=T), max_date=max(data[,date], na.rm=T), param_lab="pH", criteria){
+profileHeatMap=function(data, parameter, depth="Depth_m", param_units, depth_units="m", date="ActivityStartDate", show_dates=TRUE, min_date=min(data[,date], na.rm=T), max_date=max(data[,date], na.rm=T), param_lab="pH", criteria){
 
 	##############
 	#load("assessed_profs.rdata")
@@ -32,8 +32,11 @@ profileHeatMap=function(data, parameter, depth="Depth_m", param_units, depth_uni
 	#min_date=min(data[,date], na.rm=T)
 	#max_date=max(data[,date], na.rm=T)
 	#criteria=c(20,27)
+	#profileHeatMap(assessed_profs$profiles_wide[assessed_profs$profiles_wide$IR_MLID=="UTAHDWQ_WQX-5914010",],parameter="Temp_degC",param_units="deg C",param_lab="Temperature",depth="Depth_m",depth_units="m")
 	##########
 	
+	data[,date]=as.Date(data[,date])
+	if(missing(criteria)){criteria=c(0,10,15,20,25,30)}
 	data=data[!is.na(data[,parameter]),]
 	if(any(is.na(data[,parameter]))){
 		stop("Missing values in profile for selected parameter")
@@ -44,11 +47,13 @@ profileHeatMap=function(data, parameter, depth="Depth_m", param_units, depth_uni
 	if(show_dates==TRUE){
 		par(mar=c(8.1,4.1,4.1,2.1))
 		filled.contour(int,color.palette=topo.colors,xlim=c(min_date-1,max_date+1),ylim=rev(range(data[,depth])),
+			ylab=paste0("Depth (",depth_units,")"), main=paste0(param_lab," (",param_units,")"),
 			plot.axes = { contour(int, levels = criteria, drawlabels = TRUE, axes = FALSE, frame.plot = FALSE, add = TRUE);
 									axis(1,at=unique(data[,date]),labels=unique(data[,date]),par(las=2)); axis(2) } )
 			
 	}else{
 		filled.contour(int,color.palette=topo.colors,xlim=c(min_date-1,max_date+1),ylim=rev(range(data[,depth])),
+			ylab=paste0("Depth (",depth_units,")"), main=paste0(param_lab," (",param_units,")"),
 			plot.axes = { contour(int, levels = criteria, 
 				drawlabels = TRUE, axes = FALSE, 
                 frame.plot = FALSE, add = TRUE);
