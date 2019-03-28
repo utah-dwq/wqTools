008f85323e1e2186fe0a22cc37bf342296f515b4
jakevl <39382528+jakevl@users.noreply.github.com>
2019-03-28 11:29:10 -0600

    small readAWQMS updates

--- R/readAWQMS.r
+++ R/readAWQMS.r
@@ -4,15 +4,14 @@
 #' All arguments except type are optional, but at least one must be provided to limit download size and prevent errors connecting to AWQMS.
 #' Note that some, but not all, special characters have been accounted for (e.g. spaces, fore-slashes, and commas in characteristic names and site types).
 #' @param type Data type to read. One of "projects", "sites", "metrics", "results", "continuous_results", "indexes", or "beach_actions".
+#' @param unnest_results If TRUE (default) and type=="results", the result data frame is unnested from the query return object.
 #' @param start_date Query start date in "mm/dd/yyyy" format.
 #' @param end_date Query end date in "mm/dd/yyyy" format.
-#' @param unnest_results If TRUE (default) and type=="results", the result data frame is unnested from the query return object.
-#' @param ... additional arguments to be passed to AWQMS query path. See http://www.awqms.org/files/AWQMS_Training/AWQMS%20REST%20Web%20Services%20User%20Guide.pdf for optional arguments.
+#' @param ... additional arguments to be passed to AWQMS query path. See http://awqms.org/node/2 --> AWQMS Tutorials - Web Services for optional arguments.
+#' @return A data frame of AWQMS data
 #' @importFrom plyr ldply
 #' @importFrom jsonlite fromJSON
 #' @importFrom tidyr unnest
-
-#' @return A data frame of AWQMS data
 #' @examples
 #' # Read a couple of sites
 #' sites=readAWQMS(type='sites', MonitoringLocationIdentifiersCsv=c("4900440","4900470"))
@@ -30,6 +29,8 @@
 #' 
 #' # Read Utah DWQ projects
 #' projects=readAWQMS(type="projects",OrganizationIdentifiersCsv="UTAHDWQ_WQX")
+#' # Read state-wide TDS
+#' ut_tds=readAWQMS(type='results', start_date="01/01/2016", end_date="12/31/2018", StateCode="UT", Characteristic=c("Total dissolved solids"))
 
 
 #' @export
@@ -73,8 +74,10 @@ substrRight <- function(x, n){
 substrRight(names(args),3)=="Csv"
 
 csv_args=args[substrRight(names(args),3)=="Csv"]
-for(n in 1:length(csv_args)){
-	csv_args[n]=paste0(noquote(csv_args[[n]]),collapse=',')
+if(length(csv_args)>0){
+	for(n in 1:length(csv_args)){
+		csv_args[n]=paste0(noquote(csv_args[[n]]),collapse=',')
+	}
 }
 
 args=args[substrRight(names(args),3)!="Csv"]
