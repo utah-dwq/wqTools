4aaec721769d8790382a0d2a3fbaca29ca30059e
jakevl <39382528+jakevl@users.noreply.github.com>
2019-04-09 17:11:54 -0600

    Added Utah state boundary polygon, documentation

---
+++ R/ut_poly.r
@@ -0,0 +1,6 @@
+#' Utah state boundary, excluding tribal jurisdictions (unofficial)
+#'
+#' Polygon showing unofficial boundaries of the State of Utah, excluding tribal jurisdictions
+#'
+#' @format An sf type polygon shapefile
+"ut_poly"
