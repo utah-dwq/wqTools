210f9c20be048988e3206c323779093015345509
jakevl <39382528+jakevl@users.noreply.github.com>
2019-11-14 12:25:06 -0700

    Update convertUnits.r

--- R/convertUnits.r
+++ R/convertUnits.r
@@ -38,7 +38,7 @@ convertUnits=function(x, input_units, target_units='target_unit', value_var, con
 	conv_x$conversion_flag[is.na(conv_x$conversion_factor) & toupper(conv_x[,input_units])==toupper(conv_x[,target_units])] = "ACCEPT"
 	conv_x$conversion_factor[is.na(conv_x$conversion_factor) & toupper(conv_x[,input_units])==toupper(conv_x[,target_units])] = 1
 	conv_x$conv_val_col = conv_x[,value_var] * conv_x[,'conversion_factor']
-	if(any(is.na(conv_x$conv_val_col))){
+	if(any(is.na(conv_x$conv_val_col) & !is.na(conv_x[,value_var]))){
 		warning('Unable to convert one or more unit combinations. These values are expressed as NA.')
 	}
 	
