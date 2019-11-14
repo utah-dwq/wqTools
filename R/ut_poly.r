b87244e3b173419d85411f2b41d2d74a2f1ae50c
Jacob Vanderlaan <jvander@utah.gov>
2019-11-14 12:16:06 -0700

    Unit conversion table update, ut_poly doc update, documentation, manual

--- R/ut_poly.r
+++ R/ut_poly.r
@@ -1,6 +1,6 @@
 #' Utah state boundary, excluding tribal jurisdictions (unofficial)
 #'
-#' Polygon showing unofficial boundaries of the State of Utah, excluding tribal jurisdictions
+#' Polygon showing unofficial boundaries of the State of Utah, excluding tribal jurisdictions. This is an unofficial representation used to screen sites for the integrated report only.
 #'
 #' @format An sf type polygon shapefile
 "ut_poly"
