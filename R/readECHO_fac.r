20cb609bfb057dfc53038bf7cebda7fe7a6ab37a
Jacob Vanderlaan <jvander@utah.gov>
2020-10-20 11:07:11 -0600

    Update readECHO_fac.r

--- R/readECHO_fac.r
+++ R/readECHO_fac.r
@@ -3,6 +3,7 @@
 #' This function extracts facility information from EPA ECHO based on argument inputs.
 #' @param ... Additional arguments to be passed to ECHO query path. See https://echo.epa.gov/tools/web-services/facility-search-water#!/Facility_Information/get_cwa_rest_services_get_facility_info for optional arguments for facilities. Note that arguments for output are ignored.
 #' @return A data frame of EPA ECHO facility information
+#' @param df Logical. If TRUE, extract geometry coordinates from list format and return as columns in data frame.
 #' @importFrom jsonlite fromJSON
 #' @examples
 #' # Read facility locations in Utah
@@ -13,7 +14,7 @@
 #' two_fac
 
 #' @export
-readECHO_fac<-function(...){
+readECHO_fac<-function(..., df=FALSE){
 args=list(...)
 
 #args=list(p_st="ut", p_pid=c("UT0021717","UT0020834","UT0020109","UT0020427"))
@@ -34,6 +35,14 @@ print("Querying facility geometries...")
 fac_geoJSON=jsonlite::fromJSON(geoJSON_path)
 result=fac_geoJSON$features
 print("Read facilities complete...")
+
+if(df){ # Convert from list dataframe to standard dataframe
+	fac_coords=do.call(rbind.data.frame,result$geometry$coordinates)
+	names(fac_coords)=c("dec_long","dec_lat") # Rename columns
+	fac_coords
+	fac_df=data.frame(result$properties, fac_coords) #Don't really need the type object, it's always all "Feature"
+	result=fac_df
+}
 
 return(result)
 
