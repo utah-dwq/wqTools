f0fda79680ced68f0cdd9f58d17aa50b5bf93502
Jacob Vanderlaan <jvander@utah.gov>
2021-02-24 08:36:47 -0700

    Generalize & simplify assignPolys

--- R/assignPolys.r
+++ R/assignPolys.r
@@ -1,25 +1,23 @@
 #' Assign spatial polygons to sites
 #'
 #' This function assigns spatial polygon data to water quality portal type site objects (or data with site information attached).
-#' @param polygon Input polygon used to assign information to sites. Must be an sf type polygon shapefile. User may choose from au_poly, bu_poly, huc8_12_poly, huc8_poly, huc12_poly,
-#' ss_poly, ut_poly, or wmu_poly
-#' @param columns Vector of columns user wishes to retain from polygon object. If columns object is not specified, function will retain all polygon columns. 
 #' @param data Input dataset. Must include latitude & longitude columns.
+#' @param polygon Polygon to assign information to sites. Must be an sf type polygon shapefile.
 #' @param lat Name of latitude column. Default matches WQP objects.
 #' @param long Name of longitude column. Default matches WQP objects.
+#' @param columns Vector of columns to retain from polygon object. If columns not specified, function will retain all columns. 
 #' @importFrom sf st_as_sf
 #' @importFrom sf st_set_crs
 #' @importFrom sf st_join
 
 #' @return Returns the input data frame with assessment unit information appended.
 #' @export
-assignPolys=function(polygon, columns, data, lat="LatitudeMeasure", long="LongitudeMeasure"){
-	poly = eval(parse(text = paste0("wqTools::",polygon)))
-	if(exists("columns")){poly=poly[,columns]}
+assignPolys=function(data, polygon, lat="LatitudeMeasure", long="LongitudeMeasure", columns){
+	if(exists("columns")){polygon=polygon[,columns]}
 	x=data
 	x=sf::st_as_sf(x, coords=c(long,lat), crs=4326, remove=F)
-	x=sf::st_set_crs(x, sf::st_crs(poly))	
-	isect=suppressMessages({suppressWarnings({sf::st_join(x, poly, left=TRUE)})})
+	x=sf::st_set_crs(x, sf::st_crs(polygon))	
+	isect=suppressMessages({suppressWarnings({sf::st_join(x, polygon, left=TRUE)})})
 	sf::st_geometry(isect)=NULL
 	return(isect)
 }
