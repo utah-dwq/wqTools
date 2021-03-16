baa401e57cdc3c90e1e19779a8eeeadf72496aef
Jacob Vanderlaan <jvander@utah.gov>
2021-03-16 14:34:32 -0600

    Add hnnc polygons

---
+++ R/hnnc_poly.r
@@ -0,0 +1,6 @@
+#' Utah's headwater numeric nutrient criteria polygons
+#'
+#' A spatial polygons representation of Utah's headwater numeric nutrient criteria.
+#'
+#' @format An sf type polygon shapefile
+"hnnc_poly"
