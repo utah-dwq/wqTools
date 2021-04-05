b29ff6b8065d156d0c5998e28ea7815fbaee2700
Jacob Vanderlaan <jvander@utah.gov>
2021-04-05 15:00:28 -0600

    Archive 2018/2020 AU polygons, set up for 2022 AU polygons

--- R/au_poly.r
+++ R/au_poly.r
@@ -1,6 +1,6 @@
 #' Utah's IR-specific assessment unit polygons
 #'
-#' Polygons containing assessment unit designations.
+#' Polygons containing assessment unit designations for the current IR cycle.
 #'
 #' @format An sf type polygon shapefile
 "au_poly"
---
+++ R/au_poly_1820.r
@@ -0,0 +1,6 @@
+#' IR-specific assessment unit polygons as used in the 2018/2020 IR
+#'
+#' Polygons containing assessment unit designations as used in the 2018/2020 IR.
+#'
+#' @format An sf type polygon shapefile
+"au_poly_1820"
