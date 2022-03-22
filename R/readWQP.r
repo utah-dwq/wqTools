dcf3e78346e1d9c457b6b52a35a1875aef239533
Jacob Vanderlaan <jvander@utah.gov>
2022-03-22 15:46:28 -0600

    Put readWQP auid arg back to bbox based query

--- R/readWQP.r
+++ R/readWQP.r
@@ -63,18 +63,18 @@ if(!missing(auid)){
 	}
 	au_poly=wqTools::au_poly
 	aus=au_poly[au_poly$ASSESS_ID %in% auid,]
-	aus_dis=sf::st_union(aus)
-	aus_center=sf::st_centroid(aus_dis)
+	#aus_dis=sf::st_union(aus)
+	#aus_center=sf::st_centroid(aus_dis)
 	bbox=sf::st_bbox(aus)
-	#bBox=paste(bbox[1], bbox[2], bbox[3], bbox[4], sep='%2C')
-	#args$bBox=bBox
-	ab=as.vector((bbox$ymax-bbox$ymin)*69/2)
-	radius=sqrt(ab^2*2)*1.1
-	center_lat=aus_center[[1]][2]
-	center_lon=aus_center[[1]][1]
-	args$lat=center_lat
-	args$long=center_lon
-	args$within=radius
+	bBox=paste(bbox[1], bbox[2], bbox[3], bbox[4], sep='%2C')
+	args$bBox=bBox
+	#ab=as.vector((bbox$ymax-bbox$ymin)*69/2)
+	#radius=sqrt(ab^2*2)*1.1
+	#center_lat=aus_center[[1]][2]
+	#center_lon=aus_center[[1]][1]
+	#args$lat=center_lat
+	#args$long=center_lon
+	#args$within=radius
 	args=args[!names(args) %in% 'auid']
 	geom_type='auid'
 }
